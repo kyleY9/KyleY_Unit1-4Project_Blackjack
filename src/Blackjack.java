0b90694d4d4f53c3feb9049977d4e38c31777875
Kyle Yang <KyleY9@nycstudents.net>
2023-11-16 22:47:12 -0500

    Just added some starter code.

--- src/Blackjack.java
+++ src/Blackjack.java
@@ -1,2 +1,4 @@
-package PACKAGE_NAME;public class Blackjack {
+public class Blackjack {
+
+    // instance variables
 }
--- src/BlackjackRunner.java
+++ src/BlackjackRunner.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class BlackjackRunner {
+import java.util.Scanner;
+
+public class BlackjackRunner {
+    public static void main(String[] args) {
+        // creating scanner object for receiving user input
+        Scanner scan = new Scanner(System.in);
+
+        //
+    }
 }
