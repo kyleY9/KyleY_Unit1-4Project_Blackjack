50fa5595977d8eac6e6b1aabf2dda0fe91c5384d
Kyle Yang <KyleY9@nycstudents.net>
2023-11-20 20:55:32 -0500

    More additions

--- src/BlackjackRunner.java
+++ src/BlackjackRunner.java
@@ -6,8 +6,12 @@ public class BlackjackRunner {
         Scanner scan = new Scanner(System.in);
 
         // introduction
-        Blackjack obj = new Blackjack();
-        System.out.println(obj.rand1to11());
-        System.out.println(obj.rand1to2());
+        System.out.print("Welcome to Blackjack\n\nYour objective is to win against the dealer, a randomized bot. Cards will be labeled 1-11.\n\"Hit\" to get another card. \"Stay\" to stay with your hand.\nIf the value of your cards go over 21, or is less than the bot's hand, then you bust (lose).\nIf you get a 21, or a value over the bot's hand, then you win!");
+        System.out.print("Au ready? ");
+        String readyOrNot = scan.nextLine();
+        while (!(readyOrNot.equals("Yes") && readyOrNot.equals("yes"))) {
+            System.out.print("I said. Are. You. Ready. ");
+            readyOrNot = scan.nextLine();
+        }
     }
 }
